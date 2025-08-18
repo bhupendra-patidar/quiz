b0dab2d6980f78ca620e7e2698dcedf576f52d37
Bhupendra Patidar <bhupendra.patidar@bklap296.bksdomain.com>
2025-08-18 16:19:36 +0530

    Updated service name

--- db/services.php
+++ db/services.php
@@ -2,13 +2,13 @@
 
 $functions = [
 
-    'local_quiz_ai' => [
+    'local_quiz_attempt_insert_graderesponse' => [
         'classname'   => 'local_quiz\\external\\externallib',
         'methodname'  => 'airesponse',
         'classpath'   => '',
         'description' => 'Insert ai response to a record into mdl_quiz_response table',
         'type'        => 'write',
         'ajax'        => false,
-        'capabilities'=> ['moodle/quiz:view']
+        //'capabilities'=> ['moodle/quiz:view']
     ],
 ];
